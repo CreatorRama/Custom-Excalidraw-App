c8038ac4f5555ab6eedc16a62858fda97dfb28f5
Creator Rama <ap410485@gmail.com>
2025-03-06 20:18:38 +0530

    remove cropped modal additional button

--- src/components/conva.jsx
+++ src/components/conva.jsx
@@ -1482,15 +1482,7 @@ const buttonClassName = useMemo(() => "m-1 text-sm md:text-base", []);
                 onCancel={handleCancelCrop}
               />
             )}
-            
-            <div className="mt-4 flex justify-end">
-              <button 
-                onClick={handleCancelCrop}
-                className="bg-gray-300 text-gray-800 px-4 py-2 rounded mr-2 hover:bg-gray-400"
-              >
-                Cancel
-              </button>
-            </div>
+          
           </div>
         </div>
       )}
